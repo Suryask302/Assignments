2082832f1cbb798966701ffb59b9cbf6b9231c33
Suryask302 <suryask302@gmail.com>
2022-07-29 00:09:43 +0530

    final

--- geektrust.js
+++ geektrust.js
@@ -1,8 +1,35 @@
 
+const fs = require("fs");
+let data = data = fs.readFileSync(process.argv[2]).toString();
 
-let fs = require('fs')
-const {main} = require('./main/main')
-let data = fs.readFileSync('./sample_input/input1.txt')
-main(data);
+let { addDate, subScrip, addTop, printInfo } = require('./routes/route')
+
+function main(dataInput) {
+
+    var inputLines = dataInput.toString().split("\n")
+    for (i = 0; i < inputLines.length; i++) {
+        if (inputLines) {
+            let input = inputLines[i].split(' ')
+            switch (input[0]) {
+                case 'START_SUBSCRIPTION':
+                    addDate(input[1].trim());
+                    break;
+                case 'ADD_SUBSCRIPTION':
+                    subScrip(input[1], input[2]);
+                    break;
+                case 'ADD_TOPUP':
+                    addTop(input[1], input[2]);
+                    break;
+                case 'PRINT_RENEWAL_DETAILS':
+                    printInfo()
+                    break;
+
+            }
+        }
+    }
+    
+}
+
+main(data)
 
 module.exports = { main }
